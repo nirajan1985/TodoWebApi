0b973e8c15469f650afb498a77da692d0712c36d
Nirajan Karki <nirajankarki1985@gmail.com>
2023-04-26 00:27:58 +0200

    Creating standard API response

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 using TodoApi.Data;
 using TodoApi.Models;
 using TodoApi.Models.Dto;
@@ -15,33 +16,58 @@ namespace TodoApi.Controllers
     {
         private readonly ITodoRepository _dbTodo;
         private readonly IMapper _mapper;
+        protected APIResponse _response;
         public TodoController(ITodoRepository dbTodo, IMapper mapper)
         {
             _dbTodo = dbTodo;
             _mapper= mapper;
+            this._response= new();
         }
        
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task< ActionResult<IEnumerable<TodoDTO>>> GetTodos()
+        public async Task< ActionResult<APIResponse>> GetTodos()
         {
-            IEnumerable<Todo> todoList = await _dbTodo.GetAllAsync();
-            return Ok(_mapper.Map<List<TodoDTO>>(todoList));
+            try
+            {
+                IEnumerable<Todo> todoList = await _dbTodo.GetAllAsync();
+                _response.Result = _mapper.Map<List<TodoDTO>>(todoList);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch(Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
         }
 
         [HttpGet("{id:int}",Name ="GetTodo")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task< ActionResult<TodoDTO>> GetTodo(int id)
+        public async Task< ActionResult<APIResponse>> GetTodo(int id)
         {
-            if(id == 0)
+            try
             {
-                return BadRequest();
+                if (id == 0)
+                {
+                    return BadRequest();
+                }
+                var todo = await _dbTodo.GetAsync(u => u.Id == id);
+                _response.Result = _mapper.Map<TodoDTO>(todo);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
             }
-            var todo=await _dbTodo.GetAsync(u=>u.Id==id);
-            return Ok( _mapper.Map<TodoDTO>(todo));
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+
         }
 
         [HttpPost]
@@ -49,68 +75,111 @@ namespace TodoApi.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status200OK)]
 
-        public async Task< ActionResult<TodoCreateDTO>>CreateTodo([FromBody]TodoCreateDTO createDTO)
+        public async Task< ActionResult<APIResponse>>CreateTodo([FromBody]TodoCreateDTO createDTO)
         {
-            if (await _dbTodo.GetAsync(u => u.Name.ToLower() == createDTO.Name.ToLower())!=null)
+            try
             {
-                ModelState.AddModelError("CustomError", "TodoName Already exists");
-                return BadRequest(ModelState);
+                if (await _dbTodo.GetAsync(u => u.Name.ToLower() == createDTO.Name.ToLower()) != null)
+                {
+                    ModelState.AddModelError("CustomError", "TodoName Already exists");
+                    return BadRequest(ModelState);
+                }
+
+                Todo todo = _mapper.Map<Todo>(createDTO);
+
+                await _dbTodo.CreateAsync(todo);
+                _response.Result = _mapper.Map<TodoCreateDTO>(todo);
+                _response.StatusCode = HttpStatusCode.Created;
+
+
+                return CreatedAtRoute("GetTodo", new { id = todo.Id }, _response);
             }
-            
-           Todo todo= _mapper.Map<Todo>(createDTO);
-
-            await _dbTodo.CreateAsync(todo);
-            await _dbTodo.SaveAsync();  
-
-            return CreatedAtRoute("GetTodo", new {id=todo.Id},todo);
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
         }
         [HttpDelete("{id:int}",Name ="DeleteTodo")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task< IActionResult> DeleteTodo(int id)
+        public async Task<ActionResult<APIResponse>> DeleteTodo(int id)
         {
-            var todo=await _dbTodo.GetAsync(u=>u.Id== id);
+            try
+            {
+                var todo = await _dbTodo.GetAsync(u => u.Id == id);
 
-            await _dbTodo.RemoveAsync(todo);
-            await _dbTodo.SaveAsync();
-            return NoContent();
+                await _dbTodo.RemoveAsync(todo);
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
         }
 
         [HttpPut("{id:int}",Name ="UpdateTodo")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task< IActionResult> UpdateTodo(int id, [FromBody]TodoUpdateDTO updateDTO)
+        public async Task<ActionResult<APIResponse>> UpdateTodo(int id, [FromBody]TodoUpdateDTO updateDTO)
         {
-            if(id!=updateDTO.Id)
+            try
             {
-                return BadRequest();
-            }
-           
-            Todo todo=_mapper.Map<Todo>(updateDTO);
 
-            await _dbTodo.UpdateAsync(todo);
-            await _dbTodo.SaveAsync();
-            return NoContent();
+                if (id != updateDTO.Id)
+                {
+                    return BadRequest();
+                }
+
+                Todo todo = _mapper.Map<Todo>(updateDTO);
+
+                await _dbTodo.UpdateAsync(todo);
+                _response.Result = _mapper.Map<TodoUpdateDTO>(todo);
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
         }
         [HttpPatch("{id:int}",Name ="UpdatePartialTodo")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task< IActionResult> UpdatePartialTodo(int id, [FromBody] JsonPatchDocument<TodoUpdateDTO> patchDTO)
+        public async Task<ActionResult<APIResponse>> UpdatePartialTodo(int id, [FromBody] JsonPatchDocument<TodoUpdateDTO> patchDTO)
         {
-            var todo=await _dbTodo.GetAsync(u=>u.Id== id,tracked:false);
-            
-            TodoUpdateDTO updateDTO=_mapper.Map<TodoUpdateDTO>(todo);
-            patchDTO.ApplyTo(updateDTO);
+            try
+            {
+                var todo = await _dbTodo.GetAsync(u => u.Id == id, tracked: false);
 
-           
-           
-            Todo model=_mapper.Map<Todo>(updateDTO);
+                TodoUpdateDTO updateDTO = _mapper.Map<TodoUpdateDTO>(todo);
+                patchDTO.ApplyTo(updateDTO);
 
-            await _dbTodo.UpdateAsync(model);
-           await _dbTodo.SaveAsync();
 
-            return NoContent();
+
+                Todo model = _mapper.Map<Todo>(updateDTO);
+
+                await _dbTodo.UpdateAsync(model);
+                _response.Result = _mapper.Map<TodoUpdateDTO>(model);
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
         }
 
     }
